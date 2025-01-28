a0ec3f2ba088f108658dbe1d3ef6ecd6011685c0
Lilian Sevoumian <84773202+liliansev@users.noreply.github.com>
2025-01-28 02:02:13 +0100

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     echo "Bench already exists, skipping init"
@@ -6,35 +6,33 @@ if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     bench start
 else
     echo "Creating new bench..."
+    export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
+    bench init --skip-redis-config-generation frappe-bench
+    cd frappe-bench
+
+    # Use containers instead of localhost
+    bench set-mariadb-host mariadb
+    bench set-redis-cache-host redis-cache:6379
+    bench set-redis-queue-host redis-queue:6379
+    bench set-redis-socketio-host redis:6379
+
+    # Remove redis, watch from Procfile
+    sed -i '/redis/d' ./Procfile
+    sed -i '/watch/d' ./Procfile
+
+    # Install LMS app
+    bench get-app lms
+
+    # Create and configure site
+    bench new-site ${FRAPPE_SITE_NAME} \
+    --force \
+    --mariadb-root-password ${MYSQL_ROOT_PASSWORD} \
+    --admin-password ${ADMIN_PASSWORD} \
+    --no-mariadb-socket
+
+    bench --site ${FRAPPE_SITE_NAME} install-app lms
+    bench --site ${FRAPPE_SITE_NAME} clear-cache
+    bench use ${FRAPPE_SITE_NAME}
 fi
-
-export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
-
-bench init --skip-redis-config-generation frappe-bench
-
-cd frappe-bench
-
-# Use containers instead of localhost
-bench set-mariadb-host mariadb
-bench set-redis-cache-host redis:6379
-bench set-redis-queue-host redis:6379
-bench set-redis-socketio-host redis:6379
-
-# Remove redis, watch from Procfile
-sed -i '/redis/d' ./Procfile
-sed -i '/watch/d' ./Procfile
-
-bench get-app lms
-
-bench new-site lms.localhost \
---force \
---mariadb-root-password 123 \
---admin-password admin \
---no-mariadb-socket
-
-bench --site lms.localhost install-app lms
-bench --site lms.localhost set-config developer_mode 1
-bench --site lms.localhost clear-cache
-bench use lms.localhost
 
 bench start
